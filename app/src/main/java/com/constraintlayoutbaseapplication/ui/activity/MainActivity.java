512b80b52fd90b2b7aa6ab593b92de9cda898f27
bsw_lm <https://git.oschina.net/bsw_lm/mybaseapplication.git>
2017-07-07 17:02:09 +0800

    3 title

--- app/src/main/java/com/constraintlayoutbaseapplication/ui/activity/MainActivity.java
+++ app/src/main/java/com/constraintlayoutbaseapplication/ui/activity/MainActivity.java
@@ -1,7 +1,8 @@
 package com.constraintlayoutbaseapplication.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.constraintlayoutbaseapplication.R;
 import com.constraintlayoutbaseapplication.base.BaseActivity;
@@ -12,5 +13,22 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setBaseContentView(R.layout.activity_main);
+
+        //设置title文本
+        setTitle("新Title");
+        //设置返回拦截
+        getBaseBack().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("log","退不出去了吧");
+            }
+        });
+        //设置功能键，以及点击方法回调监听
+        setBaseRightIcon1(R.mipmap.more, "更多", new OnClickRightIcon1CallBack() {
+            @Override
+            public void clickRightIcon1() {
+                Log.i("Exception","这都找到我了");
+            }
+        });
     }
 }
